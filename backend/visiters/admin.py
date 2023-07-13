8e3ce2d2ed96f675838b9b719ccf8e4b8ac6b550
sds7629 <sds7629@naver.com>
2023-07-13 19:15:21 +0900

    result

--- backend/visiters/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Visiter
-# Register your models here.
-@admin.register(Visiter)
-class VisiterAdmin(admin.ModelAdmin):
-    list_display = ("age", "gender",)
--- backend/visiters/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class Visiter(models.Model):
-    class GenderChoice(models.TextChoices):
-        MALE = ("male", "Male")
-        FEMALE = ("female", "Female")
-
-    gender = models.CharField(max_length=10, choices=GenderChoice.choices)
-    age = models.PositiveIntegerField(default=0)
